fc46a15e0357b6cbac948881b3b021af9db68c72
Thomas Chia <thomas.chia@amaas.com>
2017-01-26 12:39:25 +0800

    Restructured the project a bit. AMAAS-290

--- src/transactions/Transaction.test.js
+++
@@ -1,8 +0,0 @@
-import Transaction from './Transaction.js'
-
-describe('Transaction class', () => {
-  it('should set transactionType to Trade if undefined', () => {
-    const testTrans = new Transaction(undefined, undefined)
-    expect(testTrans.transactionType).toEqual('Trade')
-  })
-})
