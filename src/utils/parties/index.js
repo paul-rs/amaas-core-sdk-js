22f1f51b8c0bad1a7144ddbf566d3a605c13f87a
Thomas Chia <thomas.chia@amaas.com>
2017-02-06 17:14:28 +0800

    Completed the switch statement to catch other Party subclasses and parse them accordingly. AMAAS-437

--- src/utils/parties/index.js
+++ src/utils/parties/index.js
@@ -1,6 +1,10 @@
 import { retrieveData } from '../network'
 import Party from '../../parties/Party/party.js'
 import Individual from '../../parties/Individual/individual.js'
+import Broker from '../../parties/Broker/broker.js'
+import Exchange from '../../parties/Exchange/exchange.js'
+import Fund from '../../parties/Fund/fund.js'
+import GovernmentAgency from '../../parties/GovernmentAgency/governmentagency.js'
 
 function getParty(AMId, partyId, callback) {
   const params = {
@@ -18,8 +22,7 @@ function getParty(AMId, partyId, callback) {
   })
 }
 
-// TODO: Parse more classes
-function _parseParty(object) {
+export function _parseParty(object) {
   let party
   switch (object.party_type) {
     case 'Individual':
@@ -32,7 +35,84 @@ function _parseParty(object) {
         description: object.description,
         addresses: object.addresses,
         emails: object.emails,
-        references: object.references
+        references: object.references,
+        createdBy: object.createdBy,
+        updatedBy: object.updatedBy,
+        createdTime: object.createdTime,
+        updatedTime: object.updatedTime,
+        version: object.version
+      })
+      break
+    case 'Broker':
+      party = new Broker({
+        assetManagerId: object.asset_manager_id,
+        partyId: object.party_id,
+        partyStatus: object.party_status,
+        partyClass: object.party_class,
+        partyType: object.party_type,
+        description: object.description,
+        addresses: object.addresses,
+        emails: object.emails,
+        references: object.references,
+        createdBy: object.createdBy,
+        updatedBy: object.updatedBy,
+        createdTime: object.createdTime,
+        updatedTime: object.updatedTime,
+        version: object.version
+      })
+      break
+    case 'Exchange':
+      party = new Exchange({
+        assetManagerId: object.asset_manager_id,
+        partyId: object.party_id,
+        partyStatus: object.party_status,
+        partyClass: object.party_class,
+        partyType: object.party_type,
+        description: object.description,
+        addresses: object.addresses,
+        emails: object.emails,
+        references: object.references,
+        createdBy: object.createdBy,
+        updatedBy: object.updatedBy,
+        createdTime: object.createdTime,
+        updatedTime: object.updatedTime,
+        version: object.version
+      })
+      break
+    case 'Fund':
+      party = new Fund({
+        assetManagerId: object.asset_manager_id,
+        partyId: object.party_id,
+        partyStatus: object.party_status,
+        partyClass: object.party_class,
+        partyType: object.party_type,
+        description: object.description,
+        addresses: object.addresses,
+        emails: object.emails,
+        references: object.references,
+        createdBy: object.createdBy,
+        updatedBy: object.updatedBy,
+        createdTime: object.createdTime,
+        updatedTime: object.updatedTime,
+        version: object.version
+      })
+      break
+    case 'GovernmentAgency':
+      party = new GovernmentAgency({
+        assetManagerId: object.asset_manager_id,
+        partyId: object.party_id,
+        partyStatus: object.party_status,
+        partyClass: object.party_class,
+        partyType: object.party_type,
+        description: object.description,
+        addresses: object.addresses,
+        emails: object.emails,
+        references: object.references,
+        createdBy: object.createdBy,
+        updatedBy: object.updatedBy,
+        createdTime: object.createdTime,
+        updatedTime: object.updatedTime,
+        version: object.version
       })
       break
     default:
@@ -45,7 +125,12 @@ function _parseParty(object) {
         description: object.description,
         addresses: object.addresses,
         emails: object.emails,
-        references: object.references
+        references: object.references,
+        createdBy: object.createdBy,
+        updatedBy: object.updatedBy,
+        createdTime: object.createdTime,
+        updatedTime: object.updatedTime,
+        version: object.version
       })
   }
   return party
