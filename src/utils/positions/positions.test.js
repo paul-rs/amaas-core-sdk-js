5313b88d603bdb00edb33f99425c09b94e8e30a4
Steve Randy Tantra <steve.randy@amaas.com>
2017-03-07 11:47:06 +0800

    Add assertions for utils/positions/positions.test.js

--- src/utils/positions/positions.test.js
+++ src/utils/positions/positions.test.js
@@ -1,9 +1,11 @@
 import { search } from './positions.js'
+import Position from '../../transactions/Positions/position.js'
 
 describe('Position search API', () => {
   it('should return an array of Position objects', callback => {
     search('asset_manager_book_id', [7, 532, 841], (error, result) => {
-      console.log(result[0])
+      expect(Array.isArray(result)).toBeTruthy()
+      expect(result[0]).toBeInstanceOf(Position)
       callback(error)
     })
   })
