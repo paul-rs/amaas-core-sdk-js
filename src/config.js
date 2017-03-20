bac3846bac618d49a8c82f7b04ae9881c195f266
Thomas Chia <thomas.chia@amaas.com>
2017-03-20 17:53:56 +0800

    Update endpoints

--- src/config.js
+++ src/config.js
@@ -1,10 +1,10 @@
 const ENDPOINTS = {
-  'assets': 'https://zc6udsq1nb.execute-api.ap-southeast-1.amazonaws.com/dev',
-  'books': 'https://smc367plfg.execute-api.ap-southeast-1.amazonaws.com/dev',
-  'monitor': 'https://wt50nd7j7l.execute-api.ap-southeast-1.amazonaws.com/dev',
-  'parties': 'https://hpihgzmxoc.execute-api.ap-southeast-1.amazonaws.com/dev',
-  'transactions': 'https://1w0gb581sl.execute-api.ap-southeast-1.amazonaws.com/dev',
-  'assetManagers': 'https://c1hes1s60m.execute-api.ap-southeast-1.amazonaws.com/dev'
+  'assets': 'https://zc6udsq1nb.execute-api.ap-southeast-1.amazonaws.com/staging',
+  'books': 'https://smc367plfg.execute-api.ap-southeast-1.amazonaws.com/staging',
+  'monitor': 'https://wt50nd7j7l.execute-api.ap-southeast-1.amazonaws.com/staging',
+  'parties': 'https://hpihgzmxoc.execute-api.ap-southeast-1.amazonaws.com/staging',
+  'transactions': 'https://1w0gb581sl.execute-api.ap-southeast-1.amazonaws.com/staging',
+  'assetManagers': 'https://c1hes1s60m.execute-api.ap-southeast-1.amazonaws.com/staging'
 }
 
 export default ENDPOINTS
