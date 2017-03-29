dc287b782a4fc3afda55230eebeb92a6aa0bbb08
Steve Randy Tantra <steve.randy@amaas.com>
2017-03-29 16:16:42 +0800

    Support promise in utils/parties

--- src/utils/parties/parties.js
+++ src/utils/parties/parties.js
@@ -25,20 +25,22 @@ export function retrieve({AMId, partyId, token}, callback) {
     resourceId: partyId,
     token
   }
-  retrieveData(params, (error, result) => {
-    if (error) {
-      callback(error)
+  let promise = retrieveData(params).then(result => {
+    if (Array.isArray(result)) {
+      result = result.map(party => _parseParty(party))
     } else {
-      if (!Array.isArray(result)) {
-        callback(null, _parseParty(result))
-        return
-      }
-      const parties = result.map(party => {
-        return _parseParty(party)
-      })
-      callback(null, parties)
+      result = _parseParty(result)
     }
+    if (typeof callback === 'function') {
+      callback(null, result)
+    }
+    return result
   })
+  if (typeof callback !== 'function') {
+    // return promise if callback is not provided
+    return promise
+  }
+  promise.catch(error => callback(error))
 }
 
 /**
@@ -53,9 +55,17 @@ export function insert({party, token}, callback) {
     data: JSON.parse(stringified),
     token
   }
-  insertData(params, (error, result) => {
-    _handleCallback(error, result, callback)
+  let promise = insertData(params).then(result => {
+    if (typeof callback === 'function') {
+      callback(null, result)
+    }
+    return result
   })
+  if (typeof callback !== 'function') {
+    // return promise if callback is not provided
+    return promise
+  }
+  promise.catch(error => callback(error))
 }
 
 /**
@@ -74,9 +84,17 @@ export function amend({party, AMId, resourceId, token}, callback) {
     data: JSON.parse(stringified),
     token
   }
-  putData(params, (error, result) => {
-    _handleCallback(error, result, callback)
+  let promise = putData(params).then(result => {
+    if (typeof callback === 'function') {
+      callback(null, result)
+    }
+    return result
   })
+  if (typeof callback !== 'function') {
+    // return promise if callback is not provided
+    return promise
+  }
+  promise.catch(error => callback(error))
 }
 
 /**
@@ -94,9 +112,17 @@ export function partialAmend({changes, AMId, resourceId, token}, callback) {
     data: changes,
     token
   }
-  patchData(params, (error, result) => {
-    _handleCallback(error, result, callback)
+  let promise = patchData(params).then(result => {
+    if (typeof callback === 'function') {
+      callback(null, result)
+    }
+    return result
   })
+  if (typeof callback !== 'function') {
+    // return promise if callback is not provided
+    return promise
+  }
+  promise.catch(error => callback(error))
 }
 
 /**
@@ -112,9 +138,17 @@ export function deactivate({AMId, resourceId, token}, callback) {
     resourceId,
     token
   }
-  deleteData(params, (error, result) => {
-    _handleCallback(error, result, callback)
+  let promise = deleteData(params).then(result => {
+    if (typeof callback === 'function') {
+      callback(null, result)
+    }
+    return result
   })
+  if (typeof callback !== 'function') {
+    // return promise if callback is not provided
+    return promise
+  }
+  promise.catch(error => callback(error))
 }
 
 export function _parseChildren(type, children) {
@@ -325,14 +359,6 @@ export function _parseParty(object) {
       })
   }
   return party
-}
-
-function _handleCallback(error, result, callback) {
-  if (error) {
-    callback(error)
-  } else {
-    callback(null, _parseParty(result))
-  }
 }
 
 // export default getParty
