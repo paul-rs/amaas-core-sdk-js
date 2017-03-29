959ef7ea3a6b9f9b2250cbdf9a71522f61e4fe6c
Steve Randy Tantra <steve.randy@amaas.com>
2017-03-29 11:55:23 +0800

    Group tests by function

--- src/utils/books/books.test.js
+++ src/utils/books/books.test.js
@@ -6,45 +6,49 @@ jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000
 let token = process.env.API_TOKEN
 
 describe('utils/books', () => {
-  test("retrieve with callback", callback => {
-    retrieve({token, AMId: 1}, (error, books) => {
-      expect(Array.isArray(books)).toBeTruthy()
-      expect(books[0]).toBeInstanceOf(Book)
-      callback(error)
+  describe('retrieve', () => {
+    test('with callback', callback => {
+      retrieve({token, AMId: 1}, (error, books) => {
+        expect(Array.isArray(books)).toBeTruthy()
+        expect(books[0]).toBeInstanceOf(Book)
+        callback(error)
+      })
+    })
+
+    test('with promise', callback => {
+      let promise = retrieve({token, AMId: 1})
+      expect(promise).toBeInstanceOf(Promise)
+      promise.then(books => {
+        expect(Array.isArray(books)).toBeTruthy()
+        expect(books[0]).toBeInstanceOf(Book)
+        callback()
+      })
     })
   })
 
-  test("retrieve with promise", callback => {
-    let promise = retrieve({token, AMId: 1})
-    expect(promise).toBeInstanceOf(Promise)
-    promise.then(books => {
-      expect(Array.isArray(books)).toBeTruthy()
-      expect(books[0]).toBeInstanceOf(Book)
-      callback()
+  describe('search', () => {
+    test('with callback', callback => {
+      search({
+        token,
+        queryKey: 'asset_manager_ids',
+        queryValue: [269]
+      }, (error, books) => {
+        expect(Array.isArray(books)).toBeTruthy()
+        callback(error)
+      })
     })
-  })
 
-  test("search with callback", callback => {
-    search({
-      token,
-      queryKey: 'asset_manager_ids',
-      queryValue: [269]
-    }, (error, books) => {
-      expect(Array.isArray(books)).toBeTruthy()
-      callback(error)
-    })
-  })
-
-  test("search with promise", callback => {
-    let promise = search({
-      token,
-      queryKey: 'asset_manager_ids',
-      queryValue: [269]
-    })
-    expect(promise).toBeInstanceOf(Promise)
-    promise.then(books => {
-      expect(Array.isArray(books)).toBeTruthy()
-      callback()
+    test('with promise', callback => {
+      let promise = search({
+        token,
+        queryKey: 'asset_manager_ids',
+        queryValue: [269]
+      })
+      expect(promise).toBeInstanceOf(Promise)
+      promise.then(books => {
+        expect(Array.isArray(books)).toBeTruthy()
+        callback()
+      })
     })
   })
 })
