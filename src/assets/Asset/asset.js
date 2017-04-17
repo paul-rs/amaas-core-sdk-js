7a7a05266ae95d84d1ff11f93653388be89fda90
Thomas Chia <thomas.chia@amaas.com>
2017-04-17 12:21:10 +0800

    Update Asset class.

--- src/assets/Asset/asset.js
+++ src/assets/Asset/asset.js
@@ -1,4 +1,6 @@
 import { AMaaSModel, Reference } from '../../core'
+import Comment from '../../children/Comment'
+import Link from '../../children/Link'
 
 /**
  * Class representing an Asset
@@ -27,18 +29,21 @@ class Asset extends AMaaSModel {
   */
   constructor({
     assetManagerId,
-    fungible,
-    assetIssuerId,
     assetId,
     assetClass='Asset',
-    assetType='Asset',
+    fungible,
+    assetIssuerId,
     assetStatus='Active',
     countryId,
     venueId,
+    currency,
+    issueDate,
     maturityDate,
     description='',
     clientId,
-    references={},
+    comments,
+    links,
+    references,
     createdBy,
     updatedBy,
     createdTime,
@@ -52,46 +57,111 @@ class Asset extends AMaaSModel {
       updatedTime,
       version
     })
+
+    Object.defineProperties(this, {
+      _issueDate: { writable: true, enumerable: false },
+      issueDate: {
+        get: () => this._issueDate,
+        set: (newIssueDate) => {
+          // Accepts string 'yyyy-mm-dd'
+          if (newIssueDate) {
+            const dArray = newIssueDate.split('-')
+            const date = new Date(dArray[0], dArray[1]-1, dArray[2])
+            this._issueDate = date
+          }
+        },
+        enumerable: true
+      },
+      _maturityDate: { writable: true, enumerable: false },
+      maturityDate: {
+        get: () => this._maturityDate,
+        set: (newMaturityDate) => {
+          // Accepts string 'yyyy-mm-dd'
+          if (newMaturityDate) {
+            const dArray = newMaturityDate.split('-')
+            const date = new Date(dArray[0], dArray[1]-1, dArray[2])
+            this._maturityDate = date
+          }
+        },
+        enumerable: true
+      },
+      _comments: { writable: true, enumerable: false },
+      comments: {
+        get: () => this._comments,
+        set: (newComments) => {
+          if (newComments) {
+            let commentsClass = {}
+            for (let name in newComments) {
+              if (newComments.hasOwnProperty(name)) {
+                commentsClass[name] = new Comment(newComments[name])
+              }
+            }
+            this._comments = commentsClass
+          } else {
+            this._comments = {}
+          }
+        },
+        enumerable: true
+      },
+      _links: { writable: true, enumerable: false },
+      links: {
+        get: () => this._links,
+        set: (newLinks) => {
+          if (newLinks) {
+            let linksClass = {}
+            for (let name in newLinks) {
+              if (newLinks.hasOwnProperty(name)) {
+                linksClass[name] = newLinks[name].map(link => {
+                  return new Link(link)
+                })
+              }
+            }
+            this._links = linksClass
+          } else {
+            this._links = {}
+          }
+        }
+      },
+      _references: { writable: true, enumerable: false },
+      references: {
+        get: () => this._references,
+        set: (newReferences) => {
+          if (newReferences) {
+            let refClass = {
+              AMaaS: new Reference({ referenceValue: assetId })
+            }
+            for (let name in newReferences) {
+              if (newReferences.hasOwnProperty(name)) {
+                refClass[name] = new Reference(newReferences[name])
+              }
+            }
+            this._references = refClass
+          } else {
+            this._references = {
+              AMaaS: new Reference({ referenceValue: assetId })
+            }
+          }
+        }, enumerable: true
+      }
+    })
+
     this.assetManagerId = assetManagerId
     this.assetId = assetId
     this.assetClass = assetClass
-    this.assetType = assetType
+    this.assetType = this.constructor.name
     this.fungible = fungible
     this.assetIssuerId = assetIssuerId
     this.assetStatus = assetStatus
     this.countryId = countryId
     this.venueId = venueId
+    this.issueDate = issueDate
     this.maturityDate = maturityDate
     this.description = description
     this.clientId = clientId
+    this.comments = comments
+    this.links = links
     this.references = references
-    this.references.AMaaS = new Reference({ referenceValue: assetId })
   }
-
-  /*
-  toJSON() {
-    return {
-      asset_manager_id: this.assetManagerId,
-      fungible: this.fungible,
-      asset_issuer_id: this.assetIssuerId,
-      asset_id: this.assetId,
-      asset_class: this.assetClass,
-      asset_type: this.assetType,
-      asset_status: this.assetStatus,
-      country_id: this.countryId,
-      venue_id: this.venueId,
-      maturity_date: this.maturityDate,
-      description: this.description,
-      client_id: this.clientId,
-      references: this.references,
-      created_by: this.createdBy,
-      updated_by: this.updatedBy,
-      created_time: this.createdTime,
-      updated_time: this.updatedTime,
-      version: this.version
-    }
-  }
-  */
 }
 
 export default Asset
--- src/assets/Asset/asset.test.js
+++ src/assets/Asset/asset.test.js
@@ -1,16 +1,98 @@
 import Asset from './asset.js'
-import Reference from '../../core/Reference/reference.js'
+import Comment from '../../children/Comment'
+import Link from '../../children/Link'
+import { Reference } from '../../core'
 
 describe('Asset', () => {
   describe('constructor', () => {
-    it('should set the AMaaS reference as assetId', () => {
+
+    it('should set issueDate correctly', () => {
+      const testAsset = new Asset({ issueDate: '2017-04-17' })
+      const issueDate = testAsset.issueDate
+      expect(issueDate).toBeInstanceOf(Date)
+      expect(issueDate.getDate()).toEqual(17)
+      expect(issueDate.getMonth()).toEqual(3)
+      expect(issueDate.getFullYear()).toEqual(2017)
+    })
+
+    it('should set maturityDate correctly', () => {
+      const testAsset = new Asset({ maturityDate: '2020-04-20' })
+      const maturityDate = testAsset.maturityDate
+      expect(maturityDate).toBeInstanceOf(Date)
+      expect(maturityDate.getDate()).toEqual(20)
+      expect(maturityDate.getMonth()).toEqual(3)
+      expect(maturityDate.getFullYear()).toEqual(2020)
+    })
+
+    it('should set comments to empty object if no Comments', () => {
+      const testAsset = new Asset({})
+      expect(testAsset.comments).toEqual({})
+    })
+
+    it('should set comments as an object of <string, Comment> type', () => {
+      const comments = {
+        'Office': {
+          commentValue: 'comment 1',
+          active: true
+        },
+        'Legal': {
+          commentValue: 'comment 2',
+          active: false
+        }
+      }
+      const testAsset = new Asset({ comments })
+      expect(testAsset.comments.Office).toBeInstanceOf(Comment)
+    })
+
+    it('should set links to empty object if no Links', () => {
+      const testAsset = new Asset({})
+      expect(testAsset.links).toEqual({})
+    })
+
+    it('should set links as an object of <string, Link> type', () => {
+      const links = {
+        'Single': [{
+          linkedId: 'testId',
+          active: true
+        }],
+        'Multiple': [{
+          linkedId: 'testId2',
+          active: true
+        }, {
+          linkedId: 'testId3',
+          active: false
+        }]
+      }
+      const testAsset = new Asset({ links })
+      expect(testAsset.links.Single[0]).toBeInstanceOf(Link)
+    })
+
+    it('should set references to the AMaaS reference even if no References are supplied', () => {
       const testAsset = new Asset({ assetId: 'testId' })
       const expectedRef = new Reference({ referenceValue: 'testId' })
-      expect(testAsset.references.AMaaS).toEqual(expectedRef)
+      expect(testAsset.references.AMaaS).toBeDefined()
+      expect(testAsset.references.AMaaS.referenceValue).toEqual('testId')
+    })
+
+    it('should set references correctly (inclduing the AMaaS reference)', () => {
+      const references = {
+        'Office': {
+          referenceValue: 'testRef1'
+        },
+        'Legal': {
+          referenceValue: 'testRef2'
+        }
+      }
+      const testAsset = new Asset({ assetId: 'assetId', references })
+      const testRef = new Reference({ referenceValue: 't' })
+      expect(testAsset.references.AMaaS).toBeDefined()
+      expect(testAsset.references.AMaaS.referenceValue).toEqual('assetId')
+      expect(testAsset.references.Office).toBeInstanceOf(Reference)
+      expect(testAsset.references.Office.referenceValue).toEqual('testRef1')
     })
 
     it('should stringify correctly', () => {
-      const testAsset = new Asset({ assetId: 'testId' })
+      const testAsset = new Asset({ assetId: 'testId', issueDate: '2017-04-17' })
       expect(JSON.parse(JSON.stringify(testAsset)).assetId).toEqual('testId')
     })
   })
