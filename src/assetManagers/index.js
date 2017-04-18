7b75862b3784cd5d546999b0112997baf7d370c7
Thomas Chia <thomas.chia@amaas.com>
2017-04-18 11:17:39 +0800

    Export assetManager

---
+++ src/assetManagers/index.js
@@ -0,0 +1,2 @@
+export { default as AssetManager } from './AssetManager/assetManager'
+export { default as Relationship } from './Relationship'
---
+++ src/exports/assetManager.js
@@ -0,0 +1,24 @@
+
+// Classes
+import {
+  AssetManager,
+  Relationship
+} from '../assetManagers'
+
+// CRUD methods
+import {
+  retrieve,
+  insert,
+  amendAM,
+  deactivate
+} from '../utils/assetManagers'
+
+// Exports
+export {
+  AssetManager,
+  Relationship,
+  retrieve,
+  insert,
+  amendAM,
+  deactivate
+}
