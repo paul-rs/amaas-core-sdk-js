9bb574593507237d9f6d68e7a04513e38ed14a1f
Thomas Chia <thomas.chia@amaas.com>
2017-04-19 17:36:55 +0800

    Fix mis-typed exports.

--- dist/index.js
+++ dist/index.js
@@ -5,27 +5,27 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.Transactions = exports.Positions = exports.Parties = exports.Books = exports.Assets = exports.AssetManagers = undefined;
 
-var _assetManager = require('../exports/assetManager');
+var _assetManager = require('./exports/assetManager');
 
 var AssetManagers = _interopRequireWildcard(_assetManager);
 
-var _assets = require('../exports/assets');
+var _assets = require('./exports/assets');
 
 var Assets = _interopRequireWildcard(_assets);
 
-var _books = require('../exports/books');
+var _books = require('./exports/books');
 
 var Books = _interopRequireWildcard(_books);
 
-var _parties = require('../exports/parties');
+var _parties = require('./exports/parties');
 
 var Parties = _interopRequireWildcard(_parties);
 
-var _positions = require('../exports/positions');
+var _positions = require('./exports/positions');
 
 var Positions = _interopRequireWildcard(_positions);
 
-var _transactions = require('../exports/transactions');
+var _transactions = require('./exports/transactions');
 
 var Transactions = _interopRequireWildcard(_transactions);
 
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import * as AssetManagers from '../exports/assetManager'
-import * as Assets from '../exports/assets'
-import * as Books from '../exports/books'
-import * as Parties from '../exports/parties'
-import * as Positions from '../exports/positions'
-import * as Transactions from '../exports/transactions'
+import * as AssetManagers from './exports/assetManager'
+import * as Assets from './exports/assets'
+import * as Books from './exports/books'
+import * as Parties from './exports/parties'
+import * as Positions from './exports/positions'
+import * as Transactions from './exports/transactions'
 
 export {
   AssetManagers,
