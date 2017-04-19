832cff8ce49971e56f5cfbc5e527aa866919653b
Thomas Chia <thomas.chia@amaas.com>
2017-04-19 14:06:00 +0800

    Amend utils/assetManager test data to be camelCased.

--- src/utils/assetManagers/assetManagers.test.js
+++ src/utils/assetManagers/assetManagers.test.js
@@ -8,9 +8,9 @@ describe('utils/assetManagers', () => {
   describe('_parseAM function', () => {
     it('should return an instance of AssetManager class', () => {
       const json = {
-        asset_manager_id: '48576',
-        client_id: 'testClient',
-        default_timezone: 'UTC',
+        assetManagerId: '48576',
+        clientId: 'testClient',
+        defaultTimezone: 'UTC',
         version: 1
       }
       const parsedAM = _parseAM(json)
