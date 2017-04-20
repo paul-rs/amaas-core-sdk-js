26d507aba89e3f89df1580e72e811ef8a67d3222
Thomas Chia <thomas.chia@amaas.com>
2017-04-20 14:35:34 +0800

    Update dist.

--- dist/exports/books.js
+++ dist/exports/books.js
@@ -3,11 +3,17 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.search = exports.retrieve = exports.Book = undefined;
+exports.api = exports.Class = undefined;
 
 var _books = require('../books');
 
+var Class = _interopRequireWildcard(_books);
+
 var _books2 = require('../utils/books');
+
+var api = _interopRequireWildcard(_books2);
+
+function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) newObj[key] = obj[key]; } } newObj.default = obj; return newObj; } }
 
 // Export
 /**
@@ -16,8 +22,7 @@ var _books2 = require('../utils/books');
  */
 
 // Classes
-exports.Book = _books.Book;
-exports.retrieve = _books2.retrieve;
-exports.search = _books2.search;
+exports.Class = Class;
+exports.api = api;
 
 // CRUD methods
