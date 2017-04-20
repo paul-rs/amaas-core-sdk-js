1551f93d06a7ed136b85835c8e1214b95c3d0648
Thomas Chia <thomas.chia@amaas.com>
2017-04-20 12:04:33 +0800

    Document return type of allocations api.

--- src/utils/allocations/allocations.js
+++ src/utils/allocations/allocations.js
@@ -15,6 +15,8 @@ import { Link } from '../../children'
  * @param {number} params.AMId - Asset Manager ID of Transaction
  * @param {string} params.resourceId - Transaction ID
  * @param {string} params.token - Authorization token
+ * @param {function} [callback] - Function of form (error, result) called on completion
+ * @returns {Promise|Array} If callback given, callback is called with array of Allocations. Otherwise returns promise which resolves with array of Allocations
  */
 export function retrieve({ AMId, resourceId, token }, callback) {
   if (!AMId || !resourceId) {
@@ -52,6 +54,7 @@ export function retrieve({ AMId, resourceId, token }, callback) {
  * @param {object} params.data - Allocation data for the Transaction of the form [ { book_id: '123', quantity: '50', transaction_id: 'XYZ' }, { book_id: '456', quantity: '50', transaction_id: 'ABC' } ]
    If transaction_id is given, the new Transaction that is created will have this as ID.
  * @param {string} params.token - Authorization token
+ * @returns {Promise|Array} ???
  */
 export function send({ AMId, resourceId, data, token }, callback) {
   if (!AMId || !resourceId) {
