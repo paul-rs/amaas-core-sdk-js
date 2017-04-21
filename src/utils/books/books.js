71647671ab8694789d2b5fd98d885a61827d0295
Thomas Chia <thomas.chia@amaas.com>
2017-04-21 11:14:50 +0800

    Add insert, amend, retire, reactivate to Books .AMAAS-683

--- src/utils/books/books.js
+++ src/utils/books/books.js
@@ -1,4 +1,4 @@
-import { retrieveData, searchData } from '../network'
+import { retrieveData, searchData, patchData, insertData } from '../network'
 import Book from './../../books/Book/book'
 
 /**
@@ -78,6 +78,142 @@ export function search({queryKey, queryValue, token}, callback) {
   })
   if (typeof callback !== 'function') {
     // return promise if callback is not provided
+    return promise
+  }
+  promise.catch(error => callback(error))
+}
+
+/**
+ * Insert a new Book into the database
+ * @function insert
+ * @memberof module:Books.api
+ * @static
+ * @param {object} params - object of parameters:
+ * @param {number} params.AMId - Asset Manager ID of the AM insering the Book
+ * @param {Asset} params.book - Book instance to insert
+ * @param {string} params.token - Authorization token
+ * @param {function} callback - Called with two arguments (error, result) on completion
+ */
+export function insert({ AMId, book, token }, callback) {
+  let data
+  if (book) {
+    data = JSON.parse(JSON.stringify(book))
+  }
+  const params = {
+    AMaaSClass: 'book',
+    AMId,
+    data,
+    token
+  }
+  let promise = insertData(params).then(result => {
+    result = _parseBook(result)
+    if (typeof callback === 'function') {
+      callback(null, result)
+    }
+    return result
+  })
+  if (typeof callback !== 'function') {
+    return promise
+  }
+  promise.catch(error => callback(error))
+}
+
+/**
+ * Amend an existing Book. WARNING: This makes a HTTP PUT request and will replace the existing Book with the one passed in
+ * @function amend
+ * @memberof module:Books.api
+ * @static
+ * @param {object} params - object of parameters:
+ * @param {Asset} params.book - Amended Book instance to PUT
+ * @param {number} params.AMId - AMId of the Book to amend
+ * @param {string} params.resourceId - Book ID of the Book to amend
+ * @param {string} params.token - Authorization token
+ * @param {function} callback - Called with two arguments (error, result) on completion
+ */
+export function amend({ book, AMId, resourceId, token }, callback) {
+  let data
+  if (book) {
+    data = JSON.parse(JSON.stringify(book))
+  }
+  const params = {
+    AMaaSClass: 'book',
+    AMId,
+    resrouceId,
+    data,
+    token
+  }
+  let promise = putData(params).then(result => {
+    result = _parseBook(result)
+    if (typeof callback === 'function') {
+      callback(null, result)
+    }
+    return result
+  })
+  if (typeof callback !== 'function') {
+    return promise
+  }
+  promise.catch(error => callback(error))
+}
+
+/**
+ * Retire a Book. This will set the Book status to 'Retired'.
+ * @function retire
+ * @memberof module:Books.api
+ * @static
+ * @param {object} params - object of parameters:
+ * @param {string} params.AMId - AMId of the Books to be retired
+ * @param {string} params.resourceId - Book ID of the Book to be retired
+ * @param {string} params.token - Authorization token
+ * @param {function} callback - Called with two arguments (error, result) on completion
+ */
+export function retire({ AMId, resourceId, token }, callback) {
+  const params = {
+    AMaaSClass: 'book',
+    AMId,
+    resourceId,
+    data: { bookStatus: 'Retired' },
+    token
+  }
+  let promise = patchData(params).then(result => {
+    result = _parseBook(result)
+    if (typeof callback === 'function') {
+      callback(null, result)
+    }
+    return result
+  })
+  if (typeof callback !== 'function') {
+    return promise
+  }
+  promise.catch(error => callback(error))
+}
+
+/**
+ * Reactivate a Book. This will set the Book status to 'Active'.
+ * @function reactivate
+ * @memberof module:Books.api
+ * @static
+ * @param {object} params - object of parameters:
+ * @param {string} params.AMId - AMId of the Books to be reactivated
+ * @param {string} params.resourceId - Book ID of the Book to be reactivated
+ * @param {string} params.token - Authorization token
+ * @param {function} callback - Called with two arguments (error, result) on completion
+ */
+export function reactivate({ AMId, resourceId, token }, callback) {
+  const params = {
+    AMaaSClass: 'book',
+    AMId,
+    resourceId,
+    data: { bookStatus: 'Active' },
+    token
+  }
+  let promise = patchData(params).then(result => {
+    result = _parseBook(result)
+    if (typeof callback === 'function') {
+      callback(null, result)
+    }
+    return result
+  })
+  if (typeof callback !== 'function') {
     return promise
   }
   promise.catch(error => callback(error))
