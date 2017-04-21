d103cd4552b5f3e270edd38223e5d45409154a32
Thomas Chia <thomas.chia@amaas.com>
2017-04-21 15:13:53 +0800

    Move Relationships to its own module.

--- src/assetManagers/index.js
+++ src/assetManagers/index.js
@@ -4,4 +4,3 @@
  */
 
 export { default as AssetManager } from './AssetManager/assetManager'
-export { default as Relationship } from './Relationship'
---
+++ src/exports/relationships.js
@@ -0,0 +1,15 @@
+/**
+ * Classes and API methods for the Relationships service
+ * @module Relationships
+ */
+
+// Classes
+import * as Class from '../relattionships'
+
+// CRUD methods
+
+
+// Exports
+export {
+  Class
+}
