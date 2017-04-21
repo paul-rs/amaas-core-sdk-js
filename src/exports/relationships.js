4a24936081eca4ad2e0f64bc85862718a71a02f4
Thomas Chia <thomas.chia@amaas.com>
2017-04-21 16:30:12 +0800

    Export the Relationships module.

--- src/exports/relationships.js
+++ src/exports/relationships.js
@@ -1,15 +1,16 @@
 /**
- * Classes and API methods for the Relationships service
+ * Classes and API methods for the Relationships service (not ready yet)
  * @module Relationships
  */
 
 // Classes
-import * as Class from '../relattionships'
+import * as Class from '../relationships'
 
 // CRUD methods
-
+import * as api from '../utils/relationships'
 
 // Exports
 export {
-  Class
+  Class,
+  api
 }
