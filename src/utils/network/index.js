a300f5213a17f8b9e86d987b373898db9915aa58
Thomas Chia <thomas.chia@amaas.com>
2017-04-25 14:33:34 +0800

    Add a stage config variable to network.

--- src/utils/network/index.js
+++ src/utils/network/index.js
@@ -1,9 +1,18 @@
-// import { baseURL } from './constants.js'
-// import * as types from '../../core/types.js'
 import ENDPOINTS from '../../config.js'
 
 import request from 'superagent'
 require('dotenv').config()
+
+let stage
+export function configureStage(config) {
+  return stage = config
+}
+export function endpoint() {
+  if (!stage) {
+    return 'https://iwe48ph25i.execute-api.ap-southeast-1.amazonaws.com/dev'
+  }
+  return `https://iwe48ph25i.execute-api.ap-southeast-1.amazonaws.com/${stage}`
+}
 
 /***
  * !This is an internal function that should not be called by the end user!
@@ -18,31 +27,40 @@ export function buildURL({ AMaaSClass, AMId, resourceId }) {
   let baseURL = ''
   switch (AMaaSClass) {
     case 'book':
-      baseURL = `${ENDPOINTS.books}/books`
+      baseURL = `${endpoint()}/book/books`
+      // baseURL = `${ENDPOINTS.books}/books`
       break
     case 'parties':
-      baseURL = `${ENDPOINTS.parties}/parties`
+      baseURL = `${endpoint()}/party/parties`
+      // baseURL = `${ENDPOINTS.parties}/parties`
       break
     case 'assetManagers':
-      baseURL = `${ENDPOINTS.assetManagers}/asset-managers`
+      baseURL = `${endpoint()}/asset-manager/asset-managers`
+      // baseURL = `${ENDPOINTS.assetManagers}/asset-managers`
       break
     case 'assets':
-      baseURL = `${ENDPOINTS.assets}/assets`
+      baseURL = `${endpoint()}/asset/assets`
+      // baseURL = `${ENDPOINTS.assets}/assets`
       break
     case 'positions':
-      baseURL = `${ENDPOINTS.transactions}/positions`
+      baseURL = `${endpoint()}/position/positions`
+      // baseURL = `${ENDPOINTS.transactions}/positions`
       break
     case 'allocations':
-      baseURL = `${ENDPOINTS.transactions}/allocations`
+      baseURL = `${endpoint()}/allocation/allocations`
+      // baseURL = `${ENDPOINTS.transactions}/allocations`
       break
     case 'netting':
-      baseURL = `${ENDPOINTS.transactions}/netting`
+      baseURL = `${endpoint()}/netting/netting`
+      // baseURL = `${ENDPOINTS.transactions}/netting`
       break
     case 'relationships':
-      baseURL = `${ENDPOINTS.assetManagers}/asset-manager-relationships`
+      baseURL = `${endpoint()}/asset-manager-relationship/asset-manager-relationships`
+      // baseURL = `${ENDPOINTS.assetManagers}/asset-manager-relationships`
       break
     case 'transactions':
-      baseURL = `${ENDPOINTS.transactions}/transactions`
+      baseURL = `${endpoint()}/transaction/transactions`
+      // baseURL = `${ENDPOINTS.transactions}/transactions`
       break
     default:
       throw new Error(`Invalid class type: ${AMaaSClass}`)
@@ -53,6 +71,24 @@ export function buildURL({ AMaaSClass, AMId, resourceId }) {
     return `${baseURL}/${AMId}`
   } else {
     return `${baseURL}/${AMId}/${resourceId}`
+  }
+}
+
+export function makeRequest({ method, url, data, token }) {
+  switch (method) {
+    case 'GET':
+      return request.get(url).set('x-api-key', token).query({ camelcase: true })
+    case 'SEARCH':
+      return request.get(url).set('x-api-key', token).query(data)
+    case 'POST':
+      return request.post(url).send(data).set('x-api-key', token).query({ camelcase: true })
+    case 'PUT':
+      return request.put(url).send(data).set('x-api-key', token).query({ camelcase: true })
+    case 'PATCH':
+      return request.patch(url).send(data).set('x-api-key', token).query({ camelcase: true })
+    case 'DELETE':
+      return request.delete(url).set('x-api-key', token).query({ camelcase: true })
+    default:
   }
 }
 
@@ -96,7 +132,8 @@ export function retrieveData({ AMaaSClass, AMId, resourceId, token }, callback) 
     callback(e)
     return
   }
-  let promise = request.get(url).set('Authorization', token).query({ camelcase: true })
+  let promise = makeRequest({ method: 'GET', url, token })
+  // let promise = request.get(url).set('x-api-key', token).query({ camelcase: true })
   if (typeof callback !== 'function') {
     // return promise if callback is not provided
     return promise.then(response => response.body)
@@ -156,7 +193,8 @@ export function insertData({ AMaaSClass, AMId, resourceId, data, token }, callba
     url,
     json: data
   }
-  let promise = request.post(url).send(data).set('Authorization', token).query({ camelcase: true })
+  let promise = makeRequest({ method: 'POST', url, data, token })
+  // let promise = request.post(url).send(data).set('x-api-key', token).query({ camelcase: true })
   if (typeof callback !== 'function') {
     // return promise if callback is not provided
     return promise.then(response => response.body)
@@ -194,7 +232,8 @@ export function putData({ AMaaSClass, AMId, resourceId, data, token }, callback)
     url,
     json: data
   }
-  let promise = request.put(url).send(data).set('Authorization', token).query({ camelcase: true })
+  let promise = makeRequest({ method: 'PUT', url, data, token })
+  // let promise = request.put(url).send(data).set('x-api-key', token).query({ camelcase: true })
   if (typeof callback !== 'function') {
     // return promise if callback is not provided
     return promise.then(response => response.body)
@@ -232,7 +271,8 @@ export function patchData({ AMaaSClass, AMId, resourceId, data, token }, callbac
     url,
     json: data
   }
-  let promise = request.patch(url).send(data).set('Authorization', token).query({ camelcase: true })
+  let promise = makeRequest({ method: 'PATCH', url, data, token })
+  // let promise = request.patch(url).send(data).set('x-api-key', token).query({ camelcase: true })
   if (typeof callback !== 'function') {
     // return promise if callback is not provided
     return promise.then(response => response.body)
@@ -266,7 +306,8 @@ export function deleteData({ AMaaSClass, AMId, resourceId, token }, callback) {
     callback(e)
     return
   }
-  let promise = request.delete(url).set('Authorization', token).query({ camelcase: true })
+  let promise = makeRequest({ method: 'DELETE', url, token })
+  // let promise = request.delete(url).set('x-api-key', token).query({ camelcase: true })
   if (typeof callback !== 'function') {
     // return promise if callback is not provided
     return promise.then(response => response.body)
@@ -308,11 +349,12 @@ export function searchData({ AMaaSClass, query, token }, callback) {
     callback(e)
     return
   }
-  let queryString = { camelcase: true }
+  let data = { camelcase: true }
   for (let i = 0; i < query.length; i++) {
     queryString[query[i].key] = query[i].values.join()
   }
-  let promise = request.get(url).set('Authorization', token).query(queryString)
+  let promise = makeRequest({ method: 'SEARCH', url, token, data })
+  // let promise = request.get(url).set('x-api-key', token).query(queryString)
   if (typeof callback !== 'function') {
     // return promise if callback is not provided
     return promise.then(response => response)
