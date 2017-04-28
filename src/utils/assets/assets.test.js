df0a9993c78d538ef355a823c85f9cc1bc79e011
Thomas Chia <thomas.chia@amaas.com>
2017-04-28 17:50:02 +0800

    Update api.Assets tests to use cognito authentication.

--- src/utils/assets/assets.test.js
+++ src/utils/assets/assets.test.js
@@ -1,68 +1,63 @@
 import uuid from 'uuid'
-
 import { retrieve, insert, amend, partialAmend, search, deactivate, reactivate } from './assets.js'
 import Asset from '../../assets/Asset/asset.js'
 import * as api from '../../exports/api'
 
 api.config({
-  stage: 'staging',
-  apiKey: process.env.API_TOKEN
+  stage: 'staging'
 })
-jasmine.DEFAULT_TIMEOUT_INTERVAL = 40000
-let token = process.env.API_TOKEN
+
+jasmine.DEFAULT_TIMEOUT_INTERVAL = 60000
 
 describe('utils/assets', () => {
   describe('retrieve', () => {
     test('with promise', callback => {
-      let promise = retrieve({token, AMId: 1})
+      let promise = retrieve({AMId: 1})
       expect(promise).toBeInstanceOf(Promise)
       promise.then(assets => {
         expect(Array.isArray(assets)).toBeTruthy()
         expect(assets[0]).toBeInstanceOf(Asset)
         callback()
       })
+      .catch(err => console.error(err))
     })
   })
 
   describe('insert', () => {
     test('with promise', () => {
-      let promise = insert({token}).catch(error => {})
+      let promise = insert({}).catch(error => {})
       expect(promise).toBeInstanceOf(Promise)
     })
 
-    // This will fail if you keep running the test with the same assetId.
-    // TODO: Change assetId to uuid?
     it.skip('should insert', done => {
       const asset = {
         description: 'testAsset',
         assetType: 'Equity',
         assetManagerId: 1,
-        assetId: 'uu'
+        assetId: uuid().substring(0, 10)
       }
-      insert({ AMId: 1, asset, token })
+      insert({ AMId: 1, asset })
         .then(res => {
           expect(res).toEqual(expect.objectContaining(asset))
           done()
         })
         .catch(err => {
-          console.log(err)
-          expect(err).toBeUndefined()
-          done()
+          console.error(err)
         })
     })
   })
 
   describe('amend', () => {
     test('with promise', () => {
-      let promise = amend({token}).catch(error => {})
+      let promise = amend({}).catch(error => {})
       expect(promise).toBeInstanceOf(Promise)
     })
     test('amends', done => {
       let f
-      retrieve({ AMId: 1, resourceId: '121JBDQM5Z', token })
+      retrieve({ AMId: 1, resourceId: '121JBDQM5Z' })
         .then(res => {
           if (res.assetStatus === 'Inactive') {
-            return reactivate({ AMId: res.assetManagerId, resourceId: res.assetId, token })
+            return reactivate({ AMId: res.assetManagerId, resourceId: res.assetId })
           } else {
             return Promise.resolve(res)
           }
@@ -70,7 +65,7 @@ describe('utils/assets', () => {
         .then(res => {
           f = res.fungible
           res.fungible = !f
-          return amend({ asset: res, AMId: res.assetManagerId, resourceId: res.assetId, token })
+          return amend({ asset: res, AMId: res.assetManagerId, resourceId: res.assetId })
         })
         .then(res => {
           expect(res.fungible).toEqual(!f)
@@ -78,26 +73,24 @@ describe('utils/assets', () => {
         })
         .catch(err => {
           console.error(err)
-          done()
         })
     })
   })
 
   describe('partialAmend', () => {
     test('with promise', () => {
-      let promise = partialAmend({token}).catch(error => {})
+      let promise = partialAmend({}).catch(error => {})
       expect(promise).toBeInstanceOf(Promise)
     })
     test('partial amends', done => {
       const desc = uuid().substring(0, 20)
-      partialAmend({ changes: { description: desc }, AMId: 1, resourceId: '121JBDQM5Z', token })
+      partialAmend({ changes: { description: desc }, AMId: 1, resourceId: '121JBDQM5Z' })
         .then(res => {
           expect(res.description).toEqual(desc)
           done()
         })
         .catch(err => {
-          expect(err).toBeUndefined()
-          done()
+          console.error(err)
         })
     })
   })
@@ -122,11 +115,10 @@ describe('utils/assets', () => {
             const aC = res.assetClass
             if (aC !== 'Asset') {
               expect(aC).toEqual('ForeignExchange')
-              done()
             } else {
               expect(aC).toEqual('Asset')
-              done()
             }
+            done()
           }
         })
         .catch(err => console.error(err))
@@ -138,7 +130,7 @@ describe('utils/assets', () => {
     const testId = '4JZ8P7AU8E'
 
     afterAll(() => {
-      reactivate({ AMId: testAMId, resourceId: testId, token })
+      reactivate({ AMId: testAMId, resourceId: testId })
         .then()
         .catch(err => {
           console.error(`Error in cleanup: Reactivating Asset ${testId} for Asset Manager ${testAMId}`)
@@ -146,27 +138,27 @@ describe('utils/assets', () => {
     })
 
     test('with promise', () => {
-      let promise = deactivate({token}).catch(error => {})
+      let promise = deactivate({}).catch(error => {})
       expect(promise).toBeInstanceOf(Promise)
     })
 
     test('deactivates an active Asset', done => {
       let status
-      retrieve({ AMId: testAMId, resourceId: testId, token })
+      retrieve({ AMId: testAMId, resourceId: testId })
         .then(res => {
           if (res.assetStatus === 'Inactive') {
             status = 'Inactive'
-            return reactivate({ AMId: res.assetManagerId, resourceId: res.assetId, token })
+            return reactivate({ AMId: res.assetManagerId, resourceId: res.assetId })
           } else {
             status = 'Active'
-            return deactivate({ AMId: res.assetManagerId, resourceId: res.assetId, token })
+            return deactivate({ AMId: res.assetManagerId, resourceId: res.assetId })
           }
         })
         .then(res => {
           if (res.assetStatus === 'Inactive') {
-            return reactivate({ AMId: res.assetManagerId, resourceId: res.assetId, token })
+            return reactivate({ AMId: res.assetManagerId, resourceId: res.assetId })
           } else {
-            return deactivate({ AMId: res.assetManagerId, resourceId: res.assetId, token })
+            return deactivate({ AMId: res.assetManagerId, resourceId: res.assetId })
           }
         })
         .then(res => {
@@ -174,8 +166,7 @@ describe('utils/assets', () => {
           done()
         })
         .catch((err) => {
-          expect(err).toBeUndefined()
-          done()
+          console.error(err)
         })
     })
   })
