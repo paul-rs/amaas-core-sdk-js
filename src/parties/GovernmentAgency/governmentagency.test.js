1e4574e0f5b342a2edb6f07d7fb8856fd994a2d4
Jiayao Li <jiayao.li@amaas.com>
2017-05-26 09:45:44 +0800

    Test cases added

--- src/parties/GovernmentAgency/governmentagency.test.js
+++ src/parties/GovernmentAgency/governmentagency.test.js
@@ -2,6 +2,21 @@ import GovernmentAgency from './governmentAgency.js'
 
 describe('GovernmentAgency', () => {
   describe('constructor', () => {
+    it('should set url', () => {
+      const testGov = new GovernmentAgency({url: 'testURL'})
+      expect(testGov.url).toEqual('testURL')
+    })
+
+    it('should set displayName', () => {
+      const testGov = new GovernmentAgency({displayName: 'testDisplayName'})
+      expect(testGov.displayName).toEqual('testDisplayName')
+    })
+
+    it('should set legalName', () => {
+      const testGov = new GovernmentAgency({displayName: 'testLegalName'})
+      expect(testGov.legalName).toEqual('testLegalName')
+    })
+
     it('should set partyClass to Organisation', () => {
       const testGov = new GovernmentAgency({})
       expect(testGov.partyClass).toEqual('Organisation')
