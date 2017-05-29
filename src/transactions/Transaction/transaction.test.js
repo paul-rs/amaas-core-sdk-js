25c613ee9ba45d7061f130c52eedefedec6e6cba
Jiayao Li <jiayao.li@amaas.com>
2017-05-29 18:42:16 +0800

    Add transactions to excel function and its test

--- src/transactions/Transaction/transaction.test.js
+++ src/transactions/Transaction/transaction.test.js
@@ -231,4 +231,44 @@ describe('Transaction class', () => {
       expect(filteredLinks.length).toEqual(0)
     })
   })
-})
+
+
+describe('exportToExcel', () => {
+    test('with callback', callback => {
+      exportToExcel({AMId: 1, bookId: 1}, (error, transactions) => {
+        expect(Array.isArray(transactions)).toBeTruthy()
+        expect(transactions[0]).toBeInstanceOf(Transaction)
+        callback(error)
+      })
+    })
+
+    test('with promise', callback => {
+      let promise = exportToExcel({AMId: 1, bookId: 1})
+      expect(promise).toBeInstanceOf(Promise)
+      promise.then(transactions => {
+        expect(Array.isArray(transactions)).toBeTruthy()
+        expect(transactions[0]).toBeInstanceOf(Transactions)
+        callback()
+      })
+    })
+
+    it('should export to excel', done => {
+      const params = {
+        AMId: 1,
+        bookId: 1
+      }
+      exportToExcel(params)
+        .then(res => {
+          if (Array.isArray(res) && res.length > 0) {
+            expect(res[0]).toBeDefined()
+            expect(res[0].assetManagerId).toEqual(1)
+          } else if (!Array.isArray(res)) {
+            expect(res.assetManagerId).toEqual(1)
+          }
+          done()
+        })
+        .catch(err => console.error(err))
+    })
+  })
+
+  })
--- src/utils/transactions/transactions.js
+++ src/utils/transactions/transactions.js
@@ -226,3 +226,46 @@ export function search({ AMId, query }, callback) {
 function _parseTransaction(t) {
   return new Transaction(t)
 }
+
+/**
+ * Pull lists of transactions to excel
+ * @function exportToExcel
+ * @memberof module: api.Transactions
+ * @static
+ * @param {object} params -Object of parameters:
+ * @param {number} params.AMId
+ */
+
+export function exportToExcel({AMId, bookId}, callback)
+{
+   const params ={
+     AMId,
+     bookId
+   }
+   //retrieve data from database by AMId/bookId 
+  /*var result = [
+                 ["Type", "Estimate"],
+                 ["Transportation", 1670]
+                 ];*/
+  var result=retrieve(params, callback);
+  //get access to excel 
+  var ctx = new exportToExcel.RequestContext();
+  Excel.run(function (ctx)
+  {
+    //create a proxy object for the sheet
+    var sheet = ctx.workbook.worksheets.getActiveWorksheet();
+    //create a proxy object for the range 
+    var range = sheet.getRange("A1:B2");//for example, in fact there are 28 colnums 
+    //assign array value to the proxy object's values property 
+    range.values=values;
+    //Synchronize teh state between Javascript proxy objects and executing instructions queued on the context
+    return ctx.syn().then(function(){
+      console.log("Done");
+    });
+  }).catch(function(error){
+    console.log("Error: "+error);
+    if (error instanceof OfficeExtension.Error){
+      console.log("Debug info: "+JSON.stringify(error.debugInfo));
+    }
+  });
+}
