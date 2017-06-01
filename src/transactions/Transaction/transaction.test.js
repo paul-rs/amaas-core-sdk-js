85e20ffe2becdd8b5f322d4bd28e8700f5c7e192
Jiayao Li <jiayao.li@amaas.com>
2017-06-01 08:58:56 +0800

    Delete exportToExcel test

--- src/transactions/Transaction/transaction.test.js
+++ src/transactions/Transaction/transaction.test.js
@@ -230,45 +230,6 @@ describe('Transaction class', () => {
       const filteredLinks = trans.links.Multiple1.filter(link => link.linkedId === 'multiLinkedTransactionId2')
       expect(filteredLinks.length).toEqual(0)
     })
-  })
-
-
-describe('exportToExcel', () => {
-    test('with callback', callback => {
-      exportToExcel({AMId: 1, bookId: 1}, (error, transactions) => {
-        expect(Array.isArray(transactions)).toBeTruthy()
-        expect(transactions[0]).toBeInstanceOf(Transaction)
-        callback(error)
-      })
-    })
-
-    test('with promise', callback => {
-      let promise = exportToExcel({AMId: 1, bookId: 1})
-      expect(promise).toBeInstanceOf(Promise)
-      promise.then(transactions => {
-        expect(Array.isArray(transactions)).toBeTruthy()
-        expect(transactions[0]).toBeInstanceOf(Transactions)
-        callback()
-      })
-    })
-
-    it('should export to excel', done => {
-      const params = {
-        AMId: 1,
-        bookId: 1
-      }
-      exportToExcel(params)
-        .then(res => {
-          if (Array.isArray(res) && res.length > 0) {
-            expect(res[0]).toBeDefined()
-            expect(res[0].assetManagerId).toEqual(1)
-          } else if (!Array.isArray(res)) {
-            expect(res.assetManagerId).toEqual(1)
-          }
-          done()
-        })
-        .catch(err => console.error(err))
-    })
-  })
+  })  
 
   })
