21776b059414e1f3deaab266ef9af7d2b93d0944
Jiayao Li <jiayao.li@amaas.com>
2017-06-01 10:23:42 +0800

    Change asset manager test case

--- src/csv_upload/csvUploadAssetManager.test.js
+++ src/csv_upload/csvUploadAssetManager.test.js
@@ -10,34 +10,39 @@ describe('csvUpload', () => {
       AMId: '1234',
       csv: 'defaultBookCloseTime, defaultTimezone, assetManagerType, clientId, assetManagerStatus, defaultBookOwnerId'
            +'\n'+'17:30:00, UTC, Accredited Investor, 1, Active, 2'
-           +'\n'+'17:30:00, UTC, Accredited Investor, 2, Active, 2'
-           +'\n'+'17:30:00, UTC, Accredited Investor, 3, Active, 2'
+           +'\n'+'17:30:00, UTC, Accredited Investor, 2, Active, 3'
+           +'\n'+'17:30:00, UTC, Accredited Investor, 3, Active, 4'
   }
 
   const data = [
-        {defaultBookCloseTime: "17:30:00",
+        {
+        defaultBookCloseTime: "17:30:00",
         defaultTimezone: "UTC",
         assetManagerType: "Accredited Investor",
         clientId: 1,
         assetManagerStatus: "Active",
         defaultBookOwnerId: 2
       },
-      {defaultBookCloseTime: "17:30:00",
+
+      {
+        defaultBookCloseTime: "17:30:00",
         defaultTimezone: "UTC",
         assetManagerType: "Accredited Investor",
         clientId: 2,
         assetManagerStatus: "Active",
-        defaultBookOwnerId: 2
+        defaultBookOwnerId: 3
       },
-      {defaultBookCloseTime: "17:30:00",
+
+      {
+        defaultBookCloseTime: "17:30:00",
         defaultTimezone: "UTC",
         assetManagerType: "Accredited Investor",
         clientId: 3,
         assetManagerStatus: "Active",
-        defaultBookOwnerId: 2
+        defaultBookOwnerId: 4
       }
       ]
-   expect(csvUpload(testParams)).toEqual(data)
+   expect(csvUpload(testParams)).toEqual(data);
   /*csvUpload(testParams).then(res =>{
     //expect(res).toEqual(expect.objectContaining(data))
     expect(res).toBe(data);
