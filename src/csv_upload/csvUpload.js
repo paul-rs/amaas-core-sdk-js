97d7b935358da0209d044e4504d6b05e081b8b85
Jiayao Li <jiayao.li@amaas.com>
2017-06-05 19:25:46 +0800

    csv_upload for links and other class instances

--- src/csv_upload/csvUpload.js
+++ src/csv_upload/csvUpload.js
@@ -1,43 +1,236 @@
+import {Address, Email} from '../parties/Children/index'
+import {Charge, Code, Comment, Link, Reference} from '../children/index'
 /**
  * !This is an internal function that should not be called by the end user!
  * 
  * Insert csv data into database
- * @function csvUpload
- * @param {string} csv 
- * @param {string} AMaaSClass: class being requested/sent (e.g. Transaction) (required)
- * @param {string} AMId: Asset Manager Id (required)
+ * @function parseCsv
+ * @param {string} csvString 
  */
-
-//var csv is the string
-export function csvUpload({csv}){
-  //convert csv string to json format
+export function parseCsv({csvString})
+{
+   //convert csv string to json format
   var insertedCsv=[];
-  var lines=csv.split("\n"); 
-  var headers=lines[0].split(", "); //find headers
-  
-  for(var i=1;i<lines.length;i++)
+  var lines=csvString.split("\n"); 
+  var headers=lines[0].split(","); //find headers
+  var objectParam=["links", "emails", "addresses", "comments", "references", "charges"]
+   
+  for(var i=1;i<lines.length;i++) //for each line in csv data string
   {
-    var obj = {}; //declare object for each header
-    var currentline=lines[i].split(", ");
-
-    for(var j=0;j<headers.length;j++)
-    {
-      if(isNaN(currentline[j]))
-      obj[headers[j]] = currentline[j];//obj is a key-value pair obj
-      else 
-      obj[headers[j]] = parseInt(currentline[j]);
-
-    }
-    insertedCsv.push(obj);
-
-    //testing
+    var obj = {}; //declare object for each line of data   
+    //for each line
     for(var j=0;j<headers.length;j++)
     {
-       console.log(Object.keys(obj)[j]+" : "+Object.values(obj)[j]);
+      if(lines[i]==undefined) {
+          obj[headers[j]]=undefined;
+          break;
+      }
+
+      if(objectParam.indexOf(headers[j]) <= -1) //true if the header is not in the objectParam
+      {
+         var firstPart=lines[i].split(/,(.+)/)[0];
+         lines[i]=lines[i].split(/,(.+)/)[1];//update lines
+        
+         if(!isNaN(firstPart))
+         obj[headers[j]] = parseInt(firstPart);//obj is a key-value pair obj
+         else if(firstPart=="true")
+         obj[headers[j]] = true;
+         else if(firstPart=="false")
+         obj[headers[j]] = false;
+         else 
+         obj[headers[j]] = firstPart;//obj[headers[j]] = currentline[j];
+      }
+      else
+      {
+        //scan the string and get the object string 
+        var position=0;
+        var counter=0;
+       
+        for(var x=0; x<lines[i].length; x++)
+        {    
+          if(lines[i].charAt(x)=="{") counter++;
+          else if(lines[i].charAt(x)=="}") counter--;
+          
+          if(counter==0) 
+          {
+            position=x;
+            break;
+          }
+        }
+        var objString=lines[i].substr(0, position+1);  
+        if(headers[j]=="links")
+        {
+           var linkString=objString;
+           var convertedObjectString=parseLink({linkString});
+           obj[headers[j]]=convertedObjectString;
+        }
+        else
+        {
+            var addressString=objString;
+            var thisHeader=headers[j];
+            var convertedObjectString=parseAddressString({addressString, thisHeader});//{addressString, header}
+            obj[headers[j]]=convertedObjectString; 
+        }
+
+        lines[i]=lines[i].substr(position+2, lines[i].length); //+2 because the comma hasn to be removed
+      }
     }
+     insertedCsv.push(obj);//result is an array of objects
+  }    
 
-  }
-
-  return insertedCsv;
+    return insertedCsv;
 
 }
+
+
+/**
+ * !This is an internal function that should not be called by the end user!
+ * 
+ * Insert csv data into database
+ * @function parseLink
+ * @param {string} linkString 
+ */
+
+/*
+"links": {"link_1": 
+                [{"active": true, "linkedId": 1}, {"active": false, "linkedId": 2}], 
+           "link_2": 
+                [{"active": false, "linkedId": 1}]
+         }
+*/
+
+
+export function parseLink({linkString})
+{
+   
+   var link={};
+   var currentLine=linkString;
+
+   while(currentLine!=null || currentLine!="}")
+   {
+     var linksForEachType=[];
+     var firstPart=currentLine.split(/](.+)/)[0];
+     if(firstPart.charAt(0)==",") firstPart = firstPart.substr(0, 0) + '{' + firstPart.substr(0 + 1);;
+     currentLine=currentLine.split(/](.+)/)[1];
+     var splitTheFirstPart=firstPart.split("[");
+     var removeBracket=splitTheFirstPart[0].split("{");
+     var links=splitTheFirstPart[1];
+     if(links==null) break;
+     
+     var removeSemi=removeBracket[1].split(":");
+     var linkType=removeSemi[0];
+    
+    while(links!=null || links!="}")
+   {
+     var subLinks={};
+     if(links.charAt(0)==",") links = links.substr(0, 0) + '' + links.substr(0 + 1);
+     var firstPartLinks=links.split(/}(.*)/)[0];//first occurence of }
+     links=links.split(/}(.*)/)[1];
+     
+     var firstPartSplit=firstPartLinks.split("{");
+     if(firstPartSplit[1]==null) break;//no more address to split
+
+     var value=firstPartSplit[1];
+     
+     if(value==null) break;
+     var elements=value.split(",");
+     //insert elements into objects
+     for(var i=0; i<elements.length; i++)
+     {
+       var eachElement=elements[i];
+       var splitEachElement=eachElement.split(":");
+       if(splitEachElement[1]=="true")
+       subLinks[splitEachElement[0]]=true;
+       else if(splitEachElement[1]=="false")
+       subLinks[splitEachElement[0]]=false;
+       else if(!isNaN(splitEachElement[1]))
+       subLinks[splitEachElement[0]]= parseInt(splitEachElement[1]);
+       else
+       subLinks[splitEachElement[0]]=splitEachElement[1];
+     }  
+     //add link instance
+     const testLink=new Link(subLinks);
+     linksForEachType.push(testLink);
+     
+   }//for each link array/link type
+  
+  link[linkType]=linksForEachType;
+
+  } //for each link object
+  
+    return link;
+}
+
+/**
+ * !This is an internal function that should not be called by the end user!
+ * 
+ * Insert csv data into database
+ * @function parseAdress
+ * @param {string} addressString 
+ * This function ca be used for all object parameters except links. 
+ * example: csvAddress: '{personal:{line_one:abc,active:true},business:{line_one:def,active:false}}'
+ * will be converted to address object 
+ */
+
+export function parseAddressString({addressString, thisHeader})
+{
+   var addressFinal={};
+   var currentLine=addressString;
+   //console.log("received: "+addressString)
+   while(currentLine!=null || currentLine!="}")
+   {
+     var firstPart=currentLine.split(/}(.+)/)[0];
+     currentLine=currentLine.split(/}(.+)/)[1];
+     if(firstPart.charAt(0)==",") firstPart = firstPart.substr(0, 0) + '{' + firstPart.substr(0 + 1);
+     
+     var firstPartSplit=firstPart.split("{");
+     if(firstPartSplit[1]==null) break;//no more address to split
+     var key=firstPartSplit[1].slice(0, firstPartSplit[1].length-1).trim();
+     var value=firstPartSplit[2];
+     var subaddress={};
+
+     if(value==undefined) break;
+     var elements=value.split(",");
+
+     for(var i=0; i<elements.length; i++)
+     {
+       var eachElement=elements[i];
+       var splitEachElement=eachElement.split(":");
+       if(splitEachElement[1]=="true")
+       subaddress[splitEachElement[0]]=true;
+       else if(splitEachElement[1]=="false")
+       subaddress[splitEachElement[0]]=false;
+       else if(!isNaN(splitEachElement[1]))
+       subaddress[splitEachElement[0]]= parseInt(splitEachElement[1]);
+       else
+       subaddress[splitEachElement[0]]=splitEachElement[1];
+     }
+      
+      //"links", "emails", "addresses", "comments", "references", "charges"
+      if(thisHeader=="addresses"){
+      const testAddress=new Address(subaddress);//create an address instance
+      addressFinal[key]=testAddress;
+      }
+      else if(thisHeader=="emails"){
+      const testAddress=new Email(subaddress);//create an address instance
+      addressFinal[key]=testAddress;
+      }
+      else if(thisHeader=="references"){
+      const testAddress=new Reference(subaddress);//create an address instance
+      addressFinal[key]=testAddress;
+      }
+      else if(thisHeader=="comments"){
+      const testAddress=new Comment(subaddress);//create an address instance
+      addressFinal[key]=testAddress;
+      }
+      else if(thisHeader=="charges"){
+      const testAddress=new Charge(subaddress);//create an address instance
+      addressFinal[key]=testAddress;
+      }
+
+   }
+   
+    return addressFinal;
+}
+
+
--- src/csv_upload/csvUpload.test.js
+++ src/csv_upload/csvUpload.test.js
@@ -1,53 +1,101 @@
-import {csvUpload } from './csvUpload'
+import nock from 'nock'
+import * as csv from './csvUpload'
+import * as funcs from './csvUpload'
+import * as api from '../exports/api'
+import { Address } from '../parties/Children/address'
+import {Charge, Code, Comment, Link, Reference} from '../children/index'
 
 describe('csvUpload', () => {
   it('should insert to database', () =>{
-   const testParams = {
-      csv: 'description, bookType, businessUnit, partyId, closeTime, timezone, assetManagerId, ownerId, baseCurrency'
-           +'\n'+'RRN4WVXI1F3YA1IGMKZF, Trading, TestUnit, A1UNKOYGGR, 18:00:00, Asia/Tokyo, '+1234+', 50SJMSPK7A, USD'
-           +'\n'+'RRN4WVXI1F3YA1IGMKZF, Trading, TestUnit, A1UNKOYGGR, 18:00:00, Asia/Tokyo, '+1234+', 50SJMSPK7A, EURO'
-           +'\n'+'RRN4WVXI1F3YA1IGMKZF, Trading, TestUnit, A1UNKOYGGR, 18:00:00, Asia/Tokyo, '+1234+', 50SJMSPK7A, USD'
+  //test csv string
+  const testParams = {
+      csvString: 'partyId,addresses,links,assetManagerId,comments'
+      +'\n'+'A1UNKOYGGR,{personal:{lineOne:abc,lineTwo:linetwotest,active:true},business:{lineOne:def,active:false},legel:{lineOne:def,active:false}},{link_1:[{linkedId:1,active:true},{linkedId:2,active:false}],link_2:[{linkedId:1,active:false}]},1234,{commentType:{commentValue:testComment,active:true,updatedBy:dgfdsf,createdTime:05-06-2017,version:1}}'
+      +'\n'+'A1UNKOYGGR,{personal:{lineOne:abc,lineTwo:linetwotest,active:true},business:{lineOne:def,active:false},legel:{lineOne:def,active:false}},{link_1:[{linkedId:1,active:true},{linkedId:2,active:false}],link_2:[{linkedId:1,active:false}]},1234,{commentType:{commentValue:testComment,active:true,updatedBy:dgfdsf,createdTime:05-06-2017,version:1}}'
+      ,
+      header: "addresses"
+  }
+
+
+ const datatest=[{
+                "addresses": 
+                       {"business": {"active": false, "addressPrimary": false, "city": undefined, "countryId": undefined, "createdBy": "TEMP", "createdTime": undefined, "lineOne": "def", "lineTwo": undefined, "postalCode": undefined, "region": undefined, "updatedBy": "TEMP", "updatedTime": undefined, "version": 1}, 
+                        "legel": {"active": false, "addressPrimary": false, "city": undefined, "countryId": undefined, "createdBy": "TEMP", "createdTime": undefined, "lineOne": "def", "lineTwo": undefined, "postalCode": undefined, "region": undefined, "updatedBy": "TEMP", "updatedTime": undefined, "version": 1}, 
+                        "personal": {"active": true, "addressPrimary": false, "city": undefined, "countryId": undefined, "createdBy": "TEMP", "createdTime": undefined, "lineOne": "abc", "lineTwo": "linetwotest", "postalCode": undefined, "region": undefined, "updatedBy": "TEMP", "updatedTime": undefined, "version": 1}
+                      }, 
+                  "assetManagerId": 1234, 
+                  "links": {"link_1": 
+                                   [{"active": true,  "createdBy": "TEMP",  "createdTime": undefined,  "linkedId": 1, "updatedBy": "TEMP",  "updatedTime": undefined, "version": 1}, {"active": false, "createdBy": "TEMP",  "createdTime": undefined,  "linkedId": 2, "updatedBy": "TEMP",  "updatedTime": undefined, "version": 1}], 
+                            "link_2": [{"active": false, "createdBy": "TEMP",  "createdTime": undefined,  "linkedId": 1, "updatedBy": "TEMP",  "updatedTime": undefined, "version": 1}]
+                           }, 
+                  "partyId": "A1UNKOYGGR",
+                  "comments":{"commentType": {"commentValue": "testComment", "active": true, "createdBy": "TEMP", "updatedBy": "dgfdsf", "createdTime": "05-06-2017", "updatedTime": undefined, "version": 1}}
+                }, 
+                {
+                "addresses": 
+                     {"business": {"active": false, "addressPrimary": false, "city": undefined, "countryId": undefined, "createdBy": "TEMP", "createdTime": undefined, "lineOne": "def", "lineTwo": undefined, "postalCode": undefined, "region": undefined, "updatedBy": "TEMP", "updatedTime": undefined, "version": 1}, 
+                      "legel": {"active": false, "addressPrimary": false, "city": undefined, "countryId": undefined, "createdBy": "TEMP", "createdTime": undefined, "lineOne": "def", "lineTwo": undefined, "postalCode": undefined, "region": undefined, "updatedBy": "TEMP", "updatedTime": undefined, "version": 1},
+                      "personal": {"active": true, "addressPrimary": false, "city": undefined, "countryId": undefined, "createdBy": "TEMP", "createdTime": undefined, "lineOne": "abc", "lineTwo": "linetwotest", "postalCode": undefined, "region": undefined, "updatedBy": "TEMP", "updatedTime": undefined, "version": 1}
+                      }, 
+                "assetManagerId": 1234,
+                "links": {"link_1": 
+                                   [{"active": true,  "createdBy": "TEMP",  "createdTime": undefined,  "linkedId": 1, "updatedBy": "TEMP",  "updatedTime": undefined, "version": 1}, {"active": false, "createdBy": "TEMP",  "createdTime": undefined,  "linkedId": 2, "updatedBy": "TEMP",  "updatedTime": undefined, "version": 1}], 
+                            "link_2": [{"active": false, "createdBy": "TEMP",  "createdTime": undefined,  "linkedId": 1, "updatedBy": "TEMP",  "updatedTime": undefined, "version": 1}]
+                           }, 
+                 "partyId": "A1UNKOYGGR",
+                 "comments":{"commentType": {"commentValue": "testComment", "active": true, "createdBy": "TEMP", "updatedBy": "dgfdsf", "createdTime": "05-06-2017", "updatedTime": undefined, "version": 1}}
+                 }]
+  
+  //test address
+  const p1={
+      addressString: 'address'
+            +'\n'+'{personal:{line_one:abc,active:true},business:{line_one:def,active:false},legel:{line_one:def,active:false}}'
    }
 
-  const data =[ 
-       {
-        description: "RRN4WVXI1F3YA1IGMKZF",
-        bookType: "Trading",
-        businessUnit: "TestUnit",
-        partyId: "A1UNKOYGGR",
-        closeTime: "18:00:00",
-        timezone: "Asia/Tokyo",
-        assetManagerId: 1234,
-        ownerId: "50SJMSPK7A",
-        baseCurrency: "USD",
-      },
-      {
-        description: "RRN4WVXI1F3YA1IGMKZF",
-        bookType: "Trading",
-        businessUnit: "TestUnit",
-        partyId: "A1UNKOYGGR",
-        closeTime: "18:00:00",
-        timezone: "Asia/Tokyo",
-        assetManagerId: 1234,
-        ownerId: "50SJMSPK7A",
-        baseCurrency: "EURO",
-      },
-      {
-        description: "RRN4WVXI1F3YA1IGMKZF",
-        bookType: "Trading",
-        businessUnit: "TestUnit",
-        partyId: "A1UNKOYGGR",
-        closeTime: "18:00:00",
-        timezone: "Asia/Tokyo",
-        assetManagerId: 1234,
-        ownerId: "50SJMSPK7A",
-        baseCurrency: "USD",
-      }
- ]
-
-  csvUpload(testParams)
-  expect(csvUpload(testParams)).toEqual(data)
-  
-})
-  
+   const data1={
+     
+            personal: {//object
+                       line_one: "abc", //element
+                       active: true
+                     },
+            business: {
+                      line_one: "def",
+                      active: false
+                    },
+            legel: {
+              line_one: "def",
+              active:false
+            }
+   }
+   
+  //test links
+  const p2={
+      linkString: 'link'
+            +'\n'+'{link_1:[{linkedId:1,active:true},{linkedId:2,active:false}],link_2:[{linkedId:1,active:false}]}'
+   } 
+   const data2={
+    
+       link_1:
+             [
+               {linkedId: 1,
+               active: true
+               },
+               {
+                 linkedId: 2,
+                 active: false
+               }
+             ], 
+        link_2:
+              [
+               {linkedId: 1,
+               active: false
+               }
+              ]
+    }
+     //const testAddress = new Address({active: false});
+      
+     //expect(csv.parseAddressString(p1)).toEqual(data1);
+     //expect(csv.parseLink(p2)).toEqual(data2);
+     expect(csv.parseCsv(testParams)).toEqual(datatest);
+  })
 })
