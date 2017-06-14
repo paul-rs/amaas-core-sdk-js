9c7c1b9b224ca084661a7142e267b0cd6f3888c7
Jiayao Li <jiayao.li@amaas.com>
2017-06-14 17:26:59 +0800

    Add ownership class and test case

--- src/assets/RealAsset/Ownership/ownership.js
+++ src/assets/RealAsset/Ownership/ownership.js
@@ -1,6 +1,5 @@
 import {OwnerhsipObject} from './ownershipObject'
 import { AMaaSModel } from '../../../core'
-import Decimal from 'decimal.js'
 
 class Ownership {
 /**
@@ -11,35 +10,27 @@ class Ownership {
 constructor({
       ownershipObjects
     }){
-     Object.defineProperties(this, {
-     _ownershipObjects: { writable: true, enumerable: false },
-      ownershipObjects: {
-        get: () => this._ownershipObjects,
-        set: (newOwnershipObjects) => {
-          if (newOwnershipObjects) {
-            let ownershipObjects = {}
-            for (let os in ownershipObjects) {
-              if (ownershipObjects.hasOwnProperty(os)) {
-                ownershipObjects[os] = new OwnershipObject(Object.assign({}, newOwnershipObjects[os]))
-              }
-            }
-            this._ownershipObjects = ownershipObjects
-          }
-        },
-        enumerable: true
-      }
-    })
      this.ownershipObjects=ownershipObjects;
     } 
 
   checkSplit(){
    let splitSum=0; 
-   for(let splitValue in this.split)
+   
+   var arrayKey=Object.keys(this.ownershipObjects)
+   
+   for(var i=0; i<arrayKey.length; i++)
    {
-     splitSum=splitSum.plus(this.splitSum[splitValue])
+     var currentKey = arrayKey[i];
+     var getObject=this.ownershipObjects[currentKey]
+     var splitValue=this.ownershipObjects[currentKey].split
+     if(isNaN(splitValue)) return "error message: spliot has to be a number"
+     
+     splitSum=splitSum +splitValue
    }
-    
-   if(!splitSum==1) return "error message: Ownership must sum up to 100%";
+
+   
+   if(splitSum>1) return "error message: Ownership must sum up to 100%";
+   else return "Passed"
   }
 }
 
--- src/assets/RealAsset/Ownership/ownership.test.js
+++ src/assets/RealAsset/Ownership/ownership.test.js
@@ -3,7 +3,7 @@ import {OwnershipObject} from './ownershipObject'
 
 describe('Ownership', () => {
    describe('constructor', () => {
-     it('should return an error message', () => {
+    it('should return an error message', () => {
       var obj1={
           partyId: "xyz", 
           split: 0.5
@@ -15,9 +15,49 @@ describe('Ownership', () => {
       
       const owner1 = new OwnershipObject(obj1);
       const owner2 = new OwnershipObject(obj2);
-      const ownership = new Ownership({owner1, owner2});
-       
+ 
+      var owner={}
+      owner["ownershipObject1"]=owner1
+      owner["ownershipObject2"]=owner2
+      const ownership = new Ownership({ownershipObjects: owner});
+      
       expect(ownership.checkSplit()).toEqual("error message: Ownership must sum up to 100%")
-    })    
+    })
+
+    it('should return a list of ownership objects', () => {
+      var obj1={
+          partyId: "xyz", 
+          split: 0.5
+      }
+      var obj2={
+          partyId: "abc", 
+          split: 0.5
+      }
+      
+      const owner1 = new OwnershipObject(obj1);
+      const owner2 = new OwnershipObject(obj2);
+ 
+      var owner={}
+      owner["ownershipObject1"]=owner1
+      owner["ownershipObject2"]=owner2
+      const ownership = new Ownership({ownershipObjects: owner});
+
+      var receivedData={
+          ownershipObjects:
+          {
+              ownershipObject1:
+              {
+                  partyId: "xyz",
+                  split: 0.5
+              },
+              ownershipObject2:
+              {
+                  partyId: "abc",
+                  split: 0.5
+              }
+          }
+      }  
+      expect(ownership).toEqual(receivedData)
+    })      
  })
 })
